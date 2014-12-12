32744147f970ed6cfe347d50dd331e9b43b93f88
Ian C. Anderson <ian@redheadedlabs.com>
2014-12-11 20:41:40 -0500

    oscar-winning movies source

--- lib/girls_just_want_to_have_puns/phrase_service.rb
+++ lib/girls_just_want_to_have_puns/phrase_service.rb
@@ -1,5 +1,6 @@
 require 'girls_just_want_to_have_puns/phrase_sources/wikipedia_beatles_songs_phrase_source'
 require 'girls_just_want_to_have_puns/phrase_sources/wikipedia_idioms_phrase_source'
+require 'girls_just_want_to_have_puns/phrase_sources/wikipedia_oscar_winning_films_phrase_source'
 
 module GirlsJustWantToHavePuns
   class PhraseService
@@ -13,7 +14,11 @@ module GirlsJustWantToHavePuns
     end
 
     def self.sources
-      [WikipediaIdiomsPhraseSource, WikipediaBeatlesSongsPhraseSource]
+      [
+        WikipediaIdiomsPhraseSource,
+        WikipediaBeatlesSongsPhraseSource,
+        WikipediaOscarWinningFilmsPhraseSource
+      ]
     end
 
   end
